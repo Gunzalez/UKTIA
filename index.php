2fef48fecaeaa310821bd794aa3fb332dfd7850c
Segun Konibire <gunzalez@hotmail.com>
2015-04-17 23:57:57 +0100

    Setup finishes, homepage starts

--- index.php
+++ index.php
@@ -1,10 +1,35 @@
 <!doctype html>
 <html>
 <head>
-    <title>UKTIA</title>
+    <meta charset="utf-8">
+    <title>UK Tea &amp; Infusions Association - What is the UK Tea &amp; Infusions Association?</title>
+    <meta name="keywords" content="Keywords go here" />
+    <meta name="description" content="Description goes here" />
+    <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon" />
+    <link rel="icon" href="images/favicon.ico" type="image/x-icon" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0"/>
+    <link type="text/css" rel="stylesheet" href="css/uktia.css" />
 </head>
 <body class="home">
 
+    <div class="page">
+        <div class="header">
+            <h1 class="logo">
+                <a href="index.php">
+                    <img src="images/logo.png" alt="" />
+                </a>
+            </h1>
+            <ul class="aux-navigation">
+                <li class="tw"><a href="https://twitter.com/ukteacouncil" title="Twitter" target="_blank">Twitter</a></li>
+                <li class="first"><a href="/log-in" title="Log-out">Log-in</a></li>
+                <li><a href="/site-map" title="Sitemap">Sitemap</a></li>
+                <li><a href="/terms-conditions" title="Terms &amp; Conditions">T&amp;Cs</a></li>
+                <li class="last"><a href="/privacy-policy" title="Privacy Policy">Privacy Policy</a></li>
+            </ul>
+            <div class="cboth"></div>
+        </div>
+
+    </div>
 
 </body>
 </html>
