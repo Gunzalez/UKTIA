6453a186731293fc6595560f35db1ea5e584ba20
Segun Konibire <gunzalez@hotmail.com>
2015-04-19 23:46:19 +0100

    Responsive header, 65%

--- index.php
+++ index.php
@@ -13,12 +13,19 @@
 <body class="home">
 
     <div class="page">
+
         <div class="header">
+
             <h1 class="logo">
-                <a href="index.php">
-                    <img src="images/logo.png" alt="" />
-                </a>
+                <span>
+                    <span>
+                        <a href="index.php">
+                            <img src="images/logo.png" alt="" />
+                        </a>
+                    </span>
+                </span>
             </h1>
+
             <ul class="aux-navigation">
                 <li class="tw"><a href="https://twitter.com/ukteacouncil" title="Twitter" target="_blank">Twitter</a></li>
                 <li class="first"><a href="/log-in" title="Log-out">Log-in</a></li>
@@ -26,10 +33,58 @@
                 <li><a href="/terms-conditions" title="Terms &amp; Conditions">T&amp;Cs</a></li>
                 <li class="last"><a href="/privacy-policy" title="Privacy Policy">Privacy Policy</a></li>
             </ul>
+
+            <form id="search" class="search" method="post" action="/search-results">
+                <div class="row">
+                    <label class="label" for="term">Search</label>
+                    <input class="term" type="text" id="term" name="term" value="">
+                    <input class="button" type="submit" value="submit">
+                </div>
+            </form>
+
+            <div class="navigation">
+                <ul id="navigation-list" class="list">
+                    <li class="first option-one has-dropdown"><a href="/about">About UKTIA</a>
+                        <ul>
+                            <li class="first"><a href="/members-benefits">Members Benefits</a></li>
+                            <li class="last"><a href="/log-out">Members Log-out</a></li>
+                        </ul>
+                    </li>
+                    <li class="option-two has-dropdown"><a href="/pulse">The Pulse</a>
+                        <ul>
+                            <li class="first"><a href="/essential-reading">Essential Reading</a></li>
+                            <li><a href="/video-update">Video Update</a></li>
+                            <li class="last"><a href="/press">Tea in the Press</a></li>
+                        </ul>
+                    </li>
+                    <li class="option-three has-dropdown"><a href="/members-reference" class="active">Membership Reference Info</a>
+                        <ul>
+                            <li class="first"><a href="/fdf-access">Access to FDF Website</a></li>
+                            <li><a href="/annual-reports">Annual reports</a></li>
+                            <li><a href="/crisis-response">Crisis &amp; Response Procedures</a></li>
+                            <li><a href="/faqs">FAQ's</a></li>
+                            <li><a href="/industry-partners">Industry Partners</a></li>
+                            <li><a href="/key-contacts">Key Contact &amp; Profiles</a></li>
+                            <li><a href="/members">Members Directory</a></li>
+                            <li><a href="/rules-regulation">Member's Rules &amp; regulations</a></li>
+                            <li class="last"><a href="/operating-groups">Operating Groups &amp; Mandates</a></li>
+                        </ul>
+                    </li>
+                    <li class="last option-four"><a href="/actions-updates">Actions &amp; Updates</a></li>
+                </ul>
+            </div>
+
             <div class="cboth"></div>
         </div>
+        <!-- .header -->
+
+
 
     </div>
+
+    <script src="js/vendors/jquery-1.9.1.min.js" type="text/javascript"></script>
+    <script src="js/vendors/modernizr-2.6.1.min.js" type="text/javascript"></script>
+    <script src="js/uktia.js" type="text/javascript"></script>
 
 </body>
 </html>
