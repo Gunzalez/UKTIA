b68d48fd9eac4721f726a72b899bde67af33d2b3
Segun Konibire <gunzalez@hotmail.com>
2015-04-20 11:05:29 +0100

    Responsive header, 100%

--- index.php
+++ index.php
@@ -9,12 +9,17 @@
     <link rel="icon" href="images/favicon.ico" type="image/x-icon" />
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0"/>
     <link type="text/css" rel="stylesheet" href="css/uktia.css" />
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+    <link href="js/vendors/jquery.mmenu/jquery.mmenu.all.css" rel="stylesheet">
+    <script src="js/vendors/modernizr-2.6.1.min.js" type="text/javascript"></script>
 </head>
 <body class="home">
 
     <div class="page">
 
         <div class="header">
+
+            <a href="#mobile-navigation" id="mobile-navigation-button" class="mobile-navigation-button"><i class="fa fa-bars"></i></a>
 
             <h1 class="logo">
                 <span>
@@ -75,15 +80,60 @@
             </div>
 
             <div class="cboth"></div>
-        </div>
-        <!-- .header -->
+        </div><!-- .header -->
 
 
 
+
+    </div><!-- .page -->
+
+
+    <div class="mobile-navigation">
+        <nav id="mobile-navigation" class="mobile-navigation-list">
+            <ul>
+                <li>
+                    <form id="search" class="search" method="post" action="/search-results">
+                        <div class="row">
+                            <label class="label" for="term">Search</label>
+                            <input class="term" type="text" id="term" name="term" value="">
+                            <input class="button" type="submit" value="submit">
+                        </div>
+                    </form>
+                </li>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="/about">About UKTIA</a>
+                    <ul>
+                        <li><a href="/members-benefits">Members Benefits</a></li>
+                        <li><a href="/log-out">Members Log-out</a></li>
+                    </ul>
+                </li>
+                <li><a href="/pulse">The Pulse</a>
+                    <ul>
+                        <li><a href="/essential-reading">Essential Reading</a></li>
+                        <li><a href="/video-update">Video Update</a></li>
+                        <li><a href="/press">Tea in the Press</a></li>
+                    </ul>
+                </li>
+                <li><a href="/members-reference" class="active">Membership Reference Info</a>
+                    <ul>
+                        <li><a href="/fdf-access">Access to FDF Website</a></li>
+                        <li><a href="/annual-reports">Annual reports</a></li>
+                        <li><a href="/crisis-response">Crisis &amp; Response Procedures</a></li>
+                        <li><a href="/faqs">FAQ's</a></li>
+                        <li><a href="/industry-partners">Industry Partners</a></li>
+                        <li><a href="/key-contacts">Key Contact &amp; Profiles</a></li>
+                        <li><a href="/members">Members Directory</a></li>
+                        <li><a href="/rules-regulation">Member's Rules &amp; regulations</a></li>
+                        <li><a href="/operating-groups">Operating Groups &amp; Mandates</a></li>
+                    </ul>
+                </li>
+                <li><a href="/actions-updates">Actions &amp; Updates</a></li>
+            </ul>
+        </nav>
     </div>
 
     <script src="js/vendors/jquery-1.9.1.min.js" type="text/javascript"></script>
-    <script src="js/vendors/modernizr-2.6.1.min.js" type="text/javascript"></script>
+    <script src="js/vendors/jquery.mmenu/jquery.mmenu.min.all.js"></script>
     <script src="js/uktia.js" type="text/javascript"></script>
 
 </body>
