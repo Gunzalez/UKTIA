02f86073feaaf35c085ac70f22ea1850d9be1c29
Segun Konibire <gunzalez@hotmail.com>
2015-04-30 00:15:57 +0100

    Pulse 50%

--- pulse.php
+++ pulse.php
@@ -90,7 +90,292 @@
 
 
 
-            <div class="section" style="height: 600px;">
+            <div class="section">
+                <div class="wide left">
+
+                    <div class="copy-padding">
+
+                        <div class="logged-in">
+
+                            <div class="introduction">
+                                <div class="greeting">
+                                    <h2>Hello Alasdair</h2>
+                                    <p>You last logged in: <span>25/04/2015</span> at <span>4:31pm</span>.</p>
+                                    <p>On your last visit you looked at:</p>
+                                </div>
+
+                                <ul class="looked-at-list">
+                                    <li class="first">
+                                        <p class="title">UKTIA/20/15 - THIE Circular 01-2015</p>
+                                        <p class="sub-title">Viewed on 11/02/2015 at 1:09pm</p>
+                                        <a class="link" href="/actions-updates/article/uktia2015-thie-circular-012015-perchlorate">View again »</a>
+                                    </li>
+                                    <li>
+                                        <p class="title">European Tea Committee Technical...</p>
+                                        <p class="sub-title">Viewed on 18/02/2014 at 8:29pm</p>
+                                        <a class="link" href="/actions-updates/article/european-tea-committee-technical-committee">View again »</a>
+                                    </li>
+                                    <li class="last">
+                                        <p class="title">Imports Controls from China - Request...</p>
+                                        <p class="sub-title">Viewed on 02/05/2013 at 12:23pm</p>
+                                        <a class="link" href="/actions-updates/article/imports-controls-from-china-request-for-data">View again »</a>
+                                    </li>
+                                </ul>
+                            </div>
+
+                            <div class="video-update">
+                                <div class="heading">
+                                    <h3>UKTIA video update:</h3>
+                                    <a href="/video-update/archive">View archive »</a>
+                                    <div class="cboth"></div>
+                                </div>
+                                <div class="video">
+
+                                    <div class="video-responsive">
+                                        <iframe src="http://player.vimeo.com/video/95146048?title=0&amp;byline=0&amp;portrait=0&amp;color=ffffff" width="360" height="203" frameborder="0" webkitallowfullscreen="" mozallowfullscreen="" allowfullscreen=""></iframe>
+                                    </div>
+
+                                </div>
+                            </div>
+
+                            <div class="cboth"></div>
+                        </div>
+                    </div>
+
+
+                </div>
+
+                <div class="narrow right">
+
+                    <div class="calendar-box">
+                        <h3>Next Meetings:</h3>
+                        <table class="calendar small" align="center" cellpadding="0" cellspacing="0" border="0">
+                            <thead>
+                                <tr class="navigation">
+                                    <th class="prev-month">
+                                        <a href="?m=3&amp;y=2015">
+                                            «
+                                        </a>
+                                    </th>
+                                    <th colspan="5" class="current-month">
+                                        Apr 2015
+                                    </th>
+                                    <th class="next-month">
+                                        <a href="?m=5&amp;y=2015">
+                                            »
+                                        </a>
+                                    </th>
+                                </tr>
+                                <tr class="weekdays">
+                                    <th>Sun</th>
+                                    <th>Mon</th>
+                                    <th>Tue</th>
+                                    <th>Wed</th>
+                                    <th>Thu</th>
+                                    <th>Fri</th>
+                                    <th>Sat</th>
+                                </tr>
+                                </thead>
+                            <tbody>
+                            <tr>
+
+                                        <td class="day prev-next">
+                              <span class="date" title="">
+                                29
+                              </span>
+                                        </td>
+                                        <td class="day prev-next">
+                              <span class="date" title="">
+                                30
+                              </span>
+                                        </td>
+                                        <td class="day prev-next">
+                              <span class="date" title="">
+                                31
+                              </span>
+                                        </td>
+                                        <td class="day ">
+                              <span class="date" title="">
+                                1
+                              </span>
+                                        </td>
+                                        <td class="day ">
+                              <span class="date" title="">
+                                2
+                              </span>
+                                        </td>
+                                        <td class="day ">
+                              <span class="date" title="">
+                                3
+                              </span>
+                                        </td>
+                                        <td class="day ">
+                              <span class="date" title="">
+                                4
+                              </span>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td class="day ">
+                              <span class="date" title="">
+                                5
+                              </span>
+                                        </td>
+                                        <td class="day ">
+                              <span class="date" title="">
+                                6
+                              </span>
+                                        </td>
+                                        <td class="day ">
+                              <span class="date" title="">
+                                7
+                              </span>
+                                        </td>
+                                        <td class="day ">
+                              <span class="date" title="">
+                                8
+                              </span>
+                                        </td>
+                                        <td class="day ">
+                              <span class="date" title="">
+                                9
+                              </span>
+                                        </td>
+                                        <td class="day ">
+
+                              <span class="date" title="">
+                                10
+                              </span>
+                                        </td>
+                                        <td class="day ">
+                              <span class="date" title="">
+                                11
+                              </span>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td class="day ">
+                              <span class="date" title="">
+                                12
+                              </span>
+                                        </td>
+                                        <td class="day ">
+                              <span class="date" title="">
+                                13
+                              </span>
+                                        </td>
+                                        <td class="day ">
+                              <span class="date" title="">
+                                14
+                              </span>
+                                        </td>
+                                        <td class="day ">
+                              <span class="date" title="">
+                                15
+                              </span>
+                                        </td>
+                                        <td class="day ">
+                              <span class="date" title="">
+                                16
+                              </span>
+                                        </td>
+                                        <td class="day ">
+                              <span class="date" title="">
+                                17
+                              </span>
+                                        </td>
+                                        <td class="day ">
+                              <span class="date" title="">
+                                18
+                              </span>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td class="day ">
+                              <span class="date" title="">
+                                19
+                              </span>
+                                        </td>
+                                        <td class="day ">
+                              <span class="date" title="">
+                                20
+                              </span>
+                                        </td>
+                                        <td class="day ">
+                              <span class="date" title="">
+                                21
+                              </span>
+                                        </td>
+                                        <td class="day ">
+                              <span class="date" title="">
+                                22
+                              </span>
+                                        </td>
+                                        <td class="day ">
+                              <span class="date" title="">
+                                23
+                              </span>
+                                        </td>
+                                        <td class="day meeting">
+                              <span class="date" title="UKTIA Technical Operating Group Meeting">
+                                24
+                              </span>
+                                        </td>
+                                        <td class="day ">
+                              <span class="date" title="">
+                                25
+                              </span>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td class="day ">
+                              <span class="date" title="">
+                                26
+                              </span>
+                                        </td>
+                                        <td class="day ">
+                              <span class="date" title="">
+                                27
+                              </span>
+                                        </td>
+                                        <td class="day ">
+                              <span class="date" title="">
+                                28
+                              </span>
+                                        </td>
+                                        <td class="day today">
+                              <span class="date" title="Today">
+                                29
+                              </span>
+                                        </td>
+                                        <td class="day ">
+                              <span class="date" title="">
+                                30
+                              </span>
+                                        </td>
+                                        <td class="day prev-next">
+                              <span class="date" title="">
+                                1
+                              </span>
+                                        </td>
+                                        <td class="day prev-next">
+                              <span class="date" title="">
+                                2
+                              </span>
+                                        </td>
+                                    </tr>
+                    </tbody>
+                        </table>
+                        <ul class="calendar-list">
+                            <li>
+                                <span class="topic">UKTIA Technical Operating Group Meeting</span>
+                                <span class="sub-line">24th Apr 2015 - <a href="/actions-updates/article/uktia-technical-operating-group-meeting" title="UKTIA Technical Operating Group Meeting">View article</a></span>
+                            </li>
+                        </ul>
+                    </div>
+
+                </div>
+                <div class="cboth"></div>
             </div>
 
 
