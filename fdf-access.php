6949afc205b4afbc8895d5da25bf7b0f3697d9f9
Segun Konibire <gunzalez@hotmail.com>
2015-05-15 01:20:04 +0100

    Todos now done, part 2

--- fdf-access.php
+++ fdf-access.php
@@ -109,50 +109,8 @@
     </div><!-- .page -->
 
     <div class="back-to-top"><a href="#"><i class="fa fa-chevron-up"></i></a></div>
-    <div class="mobile-navigation">
-        <nav id="mobile-navigation" class="mobile-navigation-list">
-            <ul>
-                <li>
-                    <form id="search" class="search" method="post" action="search-results.php">
-                        <div class="row">
-                            <label class="label" for="term">Search</label>
-                            <input class="term" type="text" id="term" name="term" value="">
-                            <input class="button" type="submit" value="submit">
-                        </div>
-                    </form>
-                </li>
-                <li><a href="index.php">Home</a></li>
-                <li><a href="about.php">About UKTIA</a>
-                    <ul>
-                        <li><a href="benefits.php">Members Benefits</a></li>
-                        <li><a href="index.php">Members Log-out</a></li>
-                    </ul>
-                </li>
-                <li><a href="pulse.php">The Pulse</a>
-                    <ul>
-                        <li><a href="essential-reading.php">Essential Reading</a></li>
-                        <li><a href="video-update.php">Video Update</a></li>
-                        <li><a href="press.php">Tea in the Press</a></li>
-                    </ul>
-                </li>
-                <li><a href="members-reference.php">Membership Reference Info</a>
-                    <ul>
-                        <li><a href="fdf-access.php">Access to FDF Website</a></li>
-                        <li><a href="annual-reports.php">Annual reports</a></li>
-                        <li><a href="crisis-response.php">Crisis &amp; Response Procedures</a></li>
-                        <li><a href="faqs.php">FAQs</a></li>
-                        <li><a href="industry-partners.php">Industry Partners</a></li>
-                        <li><a href="key-contacts.php">Key Contact &amp; Profiles</a></li>
-                        <li><a href="members-directory.php">Members Directory</a></li>
-                        <li><a href="rules-regulations.php">Member's Rules &amp; regulations</a></li>
-                        <li><a href="operating-groups.php">Operating Groups &amp; Mandates</a></li>
-                    </ul>
-                </li>
-                <li><a href="circulars.php">Circulars</a></li>
-                <li><a href="contact.php">Contact</a></li>
-            </ul>
-        </nav>
-    </div>
+
+    <?php include('includes/mobile-nav-logged-in.php'); ?>
 
     <script src="js/vendors/jquery-1.9.1.min.js" type="text/javascript"></script>
     <script src="js/vendors/jquery.mmenu/jquery.mmenu.min.all.js"></script>
---
+++ includes/mobile-nav.php
@@ -0,0 +1,27 @@
+<div class="mobile-navigation">
+    <nav id="mobile-navigation" class="mobile-navigation-list">
+        <ul>
+            <li>
+                <form id="search" class="search" method="post" action="search-results.php">
+                    <div class="row">
+                        <label class="label" for="term">Search</label>
+                        <input class="term" type="text" id="term" name="term" value="">
+                        <input class="button" type="submit" value="submit">
+                    </div>
+                </form>
+            </li>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="about.php">About UKTIA</a>
+                <ul>
+                    <li><a href="benefits.php">Members Benefits</a></li>
+                    <li><a href="register.php">Registration</a></li>
+                    <li><a href="log-in.php">Members Log-in</a></li>
+                </ul>
+            </li>
+            <li><a href="log-in.php">The Pulse</a></li>
+            <li><a href="log-in.php">Membership Reference Info</a></li>
+            <li><a href="log-in.php">Circulars</a></li>
+            <li><a href="contact.php">Contact</a></li>
+        </ul>
+    </nav>
+</div>
