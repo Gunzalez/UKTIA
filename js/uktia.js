b6966383abd895b5e26bfed79b5033dfd0203a50
Segun Konibire <gunzalez@hotmail.com>
2015-05-16 18:48:08 +0100

    minor JavaScript refactor

--- js/uktia.js
+++ js/uktia.js
@@ -295,7 +295,7 @@
             }
 
             // smooth anchor scrolling
-            $('a[href*=#]:not([href=#])').not($('#mobile-navigation-button')).click(function() {
+            $('.archive-link a').on('click', function() {
                 if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
                     var target = $(this.hash);
                     target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
